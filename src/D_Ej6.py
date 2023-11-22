e4b323865b2410612c8b16abf3ad8207186d4efb
sbcesar <sbcesar@iesrafaelalberti.es>
2023-11-22 19:47:41 +0100

    22/11/2023

--- src/D_Ej6.py
+++ src/D_Ej6.py
@@ -1,14 +1,24 @@
 """
-
+Escribir un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
 """
 
 from borrarPantalla import borrarPantalla
+
+def mostrarInfo(d: dict):
+    clave = input("Introduce lo que quieras mostrar: ")
+    valor = input(f"Introduce tu {clave}: ")
+
+    while True:
+        
 
 
 
 def main():
     borrarPantalla()
 
+    d = {}
+    mostrarInfo(d)
+    
     
 
 if __name__ == "__main__":
